cab0be04fdfb83abdea699b3cd52492db62993ff
dro-ex <114840507+dro-ex@users.noreply.github.com>
2025-05-08 17:00:14 -0700

    Delete src directory

--- src/app/jobs/jobs.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-import Jobs from './jobs.js';
-
-describe('Jobs', () => {
-  it('<app-jobs> is an instance of Jobs', async () => {
-    const element = document.createElement('app-jobs');
-    expect(element).to.be.instanceOf(Jobs);
-  });
-});
--- src/app/tasks/tasks.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-import Tasks from './tasks.js';
-
-describe('Tasks', () => {
-  it('<app-tasks> is an instance of Tasks', async () => {
-    const element = document.createElement('app-tasks');
-    expect(element).to.be.instanceOf(Tasks);
-  });
-});
